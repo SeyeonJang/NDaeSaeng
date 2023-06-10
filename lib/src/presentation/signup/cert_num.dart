75a64400fc57e199c9728bc53ebe2531321b5373
SeyeonJang <seye0n.bliss@gmail.com>
2023-06-11 00:20:33 +0900

    design: [#2] 1-7 인증번호 입력 WF UI 수정

--- lib/src/presentation/signup/cert_num.dart
+++ lib/src/presentation/signup/cert_num.dart
@@ -1,3 +1,4 @@
+import 'package:dart_flutter/src/presentation/signup/choose_gender.dart';
 import 'package:flutter/material.dart';
 
 class CertNum extends StatefulWidget {
@@ -17,24 +18,74 @@ class _CertNumState extends State<CertNum> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Certification Number'),
+          // title: const Text('Certification Number'),
+          leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              }, icon: Icon(Icons.arrow_back)),
         ),
-        body: Padding(
-        padding: const EdgeInsets.all(18.0),
-    child: Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-    Text('Name: ${widget.userName}'),
-    Text('Phone Number: ${widget.phoneNumber}'),
-    const SizedBox(height: 16.0),
-      ElevatedButton(
-        onPressed: () {
-          // 인증번호 확인 로직 구현
-        },
-        child: const Text('확인'),
-      ),
-    ],
-    ),
+        body: Center(
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // 나중에 필요하면 쓰기 (아마 sharedPreferences 쓰면 안 쓸듯)
+            // ('Name: ${widget.userName}'),
+            // ('Phone Number: ${widget.phoneNumber}'),
+
+              const SizedBox(
+                height: 100,
+              ),
+              const Text("인증 코드", style: TextStyle(fontSize: 25)),
+              const SizedBox( height: 140, ),
+              SizedBox( // 입력 공간 Textfield
+                width: 400,
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    // labelText: 'Name',
+                      hintText: "인증코드 입력" // 서버에서 에러메시지도 만들었으면 같이 가져오기
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Text("05:00", style: TextStyle(fontSize: 20)),
+
+              const SizedBox(height: 140),
+              const Text("본인이 만 14세 이상이며, Dart 서비스의 필수 동의 항목인",
+                style: TextStyle(fontSize: 14),
+                textAlign: TextAlign.center,),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (_) => ChooseGender()));
+                    },
+                    child: Text("이용약관", style: TextStyle(fontSize: 14)),
+                  ),
+                  Text("및"),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (_) => ChooseGender()));
+                    },
+                    child: Text("개인정보 처리방침", style: TextStyle(fontSize: 14)),
+                  ),
+                  Text("에 동의하시면 계속 진행해주세요."),
+                ]
+              ),
+              const SizedBox(height: 20),
+
+              ElevatedButton(
+                onPressed: () {
+                  // 인증번호 확인 로직 구현
+
+                  // 인증번호 맞았을 때
+                  Navigator.push(context, MaterialPageRoute(builder: (_) => ChooseGender()));
+                },
+                child: const Text('동의하고 계속'),
+            ),
+          ],
+          ),
         ),
     );
   }
