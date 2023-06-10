749a573e73d3b5d51ac63d8922297c9a65c88d4f
Basak <chgy2131@naver.com>
2023-06-11 02:49:31 +0900

    refactor: [#2] LandingPage 분리

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,8 @@ import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 
 // import 'package:dart_flutter/photo_screen.dart';
 import 'package:dart_flutter/src/presentation/signup/choose_id.dart';
+import 'package:dart_flutter/src/presentation/signup/landing_page.dart';
 import 'package:dart_flutter/src/presentation/signup/choose_school.dart';
-// import 'landing_page.dart';
 
 // 랜딩페이지
 void main() {
@@ -25,34 +25,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 130,
-              ),
-              const Text("Dart", style: TextStyle(fontSize: 50)),
-              const SizedBox(height: 50,),
-              Image.asset('assets/images/dart_logo.png', width: 150, height: 150,), // 앱 로고 들어갈 자리
-              const SizedBox(
-                height: 100,
-              ),
-              const Text("누가 나를 선택했을지 보러가기", style: TextStyle(fontSize: 15)),
-              const SizedBox(height: 20,),
-              Image.asset('assets/images/main_kakao_login.png'),
-
-              // 임시 BTN **********************
-              ElevatedButton( // 다른 UI 확인용 임시 btn
-               onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (_) => ChooseSchool())),
-               child: const Text("확인용 btn"),
-              ),
-
-              // 애플 유저일 때 애플 로그인 나오도록 코드 추가하기
-            ],
-          ),
-        ),
-      ),
+      home: LandingPage(),
     );
   }
 }
