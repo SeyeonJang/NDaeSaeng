6110e1f4a09ab0a457324eebda4bd4c2c72871aa
Basak <chgy2131@naver.com>
2023-06-20 01:19:52 +0900

    feat: [#11] page view 템플릿 상단에 tab bar mock 임시 추가

--- lib/src/presentation/page_view.dart
+++ lib/src/presentation/page_view.dart
@@ -1,25 +1,56 @@
+import 'package:dart_flutter/res/size_config.dart';
 import 'package:dart_flutter/src/presentation/mypage/my_page.dart';
 import 'package:dart_flutter/src/presentation/vote/vote_list_inform_view.dart';
 import 'package:dart_flutter/src/presentation/vote/vote_pages.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class DartPageView extends StatelessWidget {
+class DartPageView extends StatefulWidget {
   const DartPageView({Key? key}) : super(key: key);
 
   @override
+  State<DartPageView> createState() => _DartPageViewState();
+}
+
+class _DartPageViewState extends State<DartPageView> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Dart PageView'),
-      ),
-      body: PageView(
-        children: const [
-          // TODO, 각 Page를 cubit으로 제어하도록 해야함
-          VoteListInformView(),
-          VotePages(),
-          MyPage(),
-        ],
+      // appBar: const TabBar(
+      //   cont
+      //   tabs: <Widget>[
+      //     Tab(text: "Darts"),
+      //     Tab(text: "Dart"),
+      //     Tab(text: "MY"),
+      //   ],
+      // ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(SizeConfig.defaultSize * 2),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  // TODO pageViewCubit 임시로 만들어서 관리
+                  Text("Darts", style: TextStyle(fontSize: SizeConfig.defaultSize * 1.8, fontWeight: FontWeight.w500, color: Colors.black)),
+                  Text("Dart", style: TextStyle(fontSize: SizeConfig.defaultSize * 1.8, fontWeight: FontWeight.w500, color: Colors.grey)),
+                  Text(" MY ", style: TextStyle(fontSize: SizeConfig.defaultSize * 1.8, fontWeight: FontWeight.w500, color: Colors.grey)),
+                ],
+              ),
+            ),
+            Expanded(
+              child: PageView(
+                children: const [
+                  // TODO, 각 Page를 cubit으로 제어하도록 해야함
+                  VoteListInformView(),
+                  VotePages(),
+                  MyPage(),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
