f7aa32f0c5d6c97058f280f878c3f9c1ef18278b
SeyeonJang <seyeon.sc82@catholic.ac.kr>
2023-07-06 00:32:48 +0900

    feat: [#27] QuickAlert 추가, 과팅 신청 완료 페이지 전환 기능 추가

--- lib/src/presentation/meet/meet_three_people_page.dart
+++ lib/src/presentation/meet/meet_three_people_page.dart
@@ -1,6 +1,8 @@
+import 'package:dart_flutter/res/size_config.dart';
 import 'package:dart_flutter/src/presentation/meet/viewmodel/meet_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:quickalert/quickalert.dart';
 
 class MeetThreePeoplePage extends StatefulWidget {
   const MeetThreePeoplePage({super.key});
@@ -10,6 +12,21 @@ class MeetThreePeoplePage extends StatefulWidget {
 }
 
 class _MeetThreePeoplePageState extends State<MeetThreePeoplePage> {
+  void showAlert() {
+    QuickAlert.show(
+        type: QuickAlertType.confirm,
+        context: context,
+        title: "3대3 과팅을 신청하시겠어요?",
+        text: "제출하면 프로필을 수정할 수 없어요!\n신중히 제출해주세요!",
+        confirmBtnText: '신청',
+        cancelBtnText: '취소',
+        onConfirmBtnTap: () {
+          BlocProvider.of<MeetCubit>(context).stepThreePeopleDone();
+          Navigator.of(context).pop(); // 다이얼로그 닫기
+        }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,7 +41,16 @@ class _MeetThreePeoplePageState extends State<MeetThreePeoplePage> {
       body: Center(
           child: Column(
             children: [
-
+              ElevatedButton(
+                onPressed: () {
+                  showAlert();
+                  // BlocProvider.of<MeetCubit>(context).stepTwoPeopleDone();
+                },
+                child: Text("신청하기", style: TextStyle(
+                  fontSize: SizeConfig.defaultSize * 1.4,
+                  fontWeight: FontWeight.w500,
+                )),
+              ),
             ],
           )
       ),
