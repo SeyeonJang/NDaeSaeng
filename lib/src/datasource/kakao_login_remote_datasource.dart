b16c8063df9c9b0e6dcc94c26e2be4a4981ae0b5
Basak <chgy2131@naver.com>
2023-07-09 12:51:10 +0900

    fix: [#2] 안드로이드 로그인 로직 개선

--- lib/src/datasource/kakao_login_remote_datasource.dart
+++ lib/src/datasource/kakao_login_remote_datasource.dart
@@ -8,38 +8,22 @@ class KakaoLoginRemoteDatasource {
     if (await isKakaoTalkInstalled()) {
       try {
         OAuthToken token = await UserApi.instance.loginWithKakaoTalk();
-        print('카카오톡으로 로그인 성공');
-        try {
-          return await getKakaoUserInform(token);
-        } catch (error) {
-          print('사용자 정보 요청 실패 $error');
-        }
+        return await getKakaoUserInform(token);
       } catch (error) {
         print('카카오톡으로 로그인 실패 $error');
 
-        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
-        // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
-        if (error is PlatformException && error.code == 'CANCELED') {
+        if (error is PlatformException && error.code == 'CANCELED') {  // 카카오 로그인 중 의도적인 취소 (예: 뒤로가기)
           throw Error();
         }
-        // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
-        try {
-          OAuthToken token = await UserApi.instance.loginWithKakaoAccount();
-          print('카카오계정으로 로그인 성공!!!');
-          return await getKakaoUserInform(token);
-        } catch (error) {
-          print('카카오계정으로 로그인 실패!!! $error');
-        }
       }
-    } else {
-      print("이 폰에는 카카오톡이 없습니다?");
-      try {
-        OAuthToken token = await UserApi.instance.loginWithKakaoAccount();
-        print('카카오계정으로 로그인 성공~~~');
-        return await getKakaoUserInform(token);
-      } catch (error) {
-        print('카카오계정으로 로그인 실패~~~ $error');
-      }
+    }
+
+    print("카카오톡으로 로그인 할 수 없습니다. (카톡에 연결된 계정이 없는 경우 또는 카톡 설치되어있지 않음)");
+    try {
+      OAuthToken token = await UserApi.instance.loginWithKakaoAccount();
+      return await getKakaoUserInform(token);
+    } catch (error) {
+      print('카카오계정으로 로그인 실패 $error');
     }
     throw Error();
   }
