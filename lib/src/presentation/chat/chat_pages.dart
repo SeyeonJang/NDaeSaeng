6bef307a90d84fd76bd45df0f604e3e56b0b0e8b
SeyeonJang <seyeon.sc82@catholic.ac.kr>
2023-09-02 21:47:02 +0900

    feat: [#238] 채팅 페이지, Cubit/State 생성

---
+++ lib/src/presentation/chat/chat_pages.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class ChatPages extends StatelessWidget {
+  const ChatPages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
