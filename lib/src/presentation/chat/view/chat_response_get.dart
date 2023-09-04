3bf4d26f4a4d85a8f7ba3254895cf2273a94170f
SeyeonJang <seyeon.sc82@catholic.ac.kr>
2023-09-04 14:41:26 +0900

    feat: [#239] 채팅 탭 내 PageView 추가

---
+++ lib/src/presentation/chat/view/chat_response_get.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ChatResponseGet extends StatelessWidget {
+  const ChatResponseGet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("ChatResponseGet");
+
+  }
+}
