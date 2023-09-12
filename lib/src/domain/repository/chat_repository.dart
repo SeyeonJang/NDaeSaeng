d634dfdeaa2595c5d6254086c856d08aa04fbf87
SeyeonJang <seyeon.sc82@catholic.ac.kr>
2023-09-12 13:08:18 +0900

    feat: [#239] 채팅방 상세보기 Entity & Repository 추가

--- lib/src/domain/repository/chat_repository.dart
+++ lib/src/domain/repository/chat_repository.dart
@@ -1,6 +1,8 @@
 import 'package:dart_flutter/src/domain/entity/matched_teams.dart';
+import 'package:dart_flutter/src/domain/entity/matched_teams_detail.dart';
 
 abstract class ChatRepository {
   Future<MatchedTeams> getMatchedTeam(int teamId);
   Future<List<MatchedTeams>> getMatchedTeams();
+  // Future<MatchedTeamsDetail> getMatchedTeamsDetail(int teamId);
 }
