a92f25666bb6b56e61021d2fef8fdfc9ca120b81
Basak <chgy2131@naver.com>
2023-09-20 00:58:32 +0900

    refactor: [#261] 대학목록을 LocalDatabase에 리소스로 사용

--- lib/src/data/repository/dart_univ_repository_impl.dart
+++ lib/src/data/repository/dart_univ_repository_impl.dart
@@ -1,9 +1,48 @@
 import 'package:dart_flutter/src/data/datasource/dart_api_remote_datasource.dart';
+import 'package:dart_flutter/src/data/datasource/university_local_datasource.dart';
+import 'package:dart_flutter/src/data/model/university_dto.dart';
 import 'package:dart_flutter/src/domain/entity/university.dart';
 import 'package:dart_flutter/src/domain/repository/university_repository.dart';
+import 'package:sqflite/sqflite.dart';
 
 class DartUniversityRepositoryImpl implements UniversityRepository {
-  Future<List<University>> getUniversities() async {
-    return (await DartApiRemoteDataSource.getUniversities()).map((university) => university.newUniversity()).toList();
+  Future<List<University>> getUniversities() async { // TODO
+    var db = await UniversityLocalDatasource.getDatabase();
+    var results = await db.rawQuery('''
+      SELECT *
+      FROM university 
+      LIMIT 10
+    ''');
+
+    List<University> universities = [];
+    for (var res in results) {
+      universities.add(UniversityDto.fromJson(res).newUniversity());
+    }
+
+    return universities;
+  }
+
+  @override
+  Future<void> deleteAll() {
+    throw Error();
+  }
+
+  @override
+  Future<University> getUniversityById(int id) async {
+    var db = await UniversityLocalDatasource.getDatabase();
+    var result = await db.query('university', where: 'id = ?', whereArgs: [id]);
+
+    if (result.isNotEmpty) {
+      return UniversityDto.fromJson(result.first).newUniversity();
+    } else {
+      throw Exception('No university found with ID $id');
+    }
+  }
+
+  @override
+  Future<void> insert(University university) async {
+    (await UniversityLocalDatasource.getDatabase()).insert(
+        'university', UniversityDto.fromUniversity(university).toJson(),
+        conflictAlgorithm: ConflictAlgorithm.replace);
   }
 }
--- lib/src/domain/repository/university_repository.dart
+++ lib/src/domain/repository/university_repository.dart
@@ -2,4 +2,7 @@ import 'package:dart_flutter/src/domain/entity/university.dart';
 
 abstract class UniversityRepository {
   Future<List<University>> getUniversities();
+  Future<void> deleteAll();
+  Future<void> insert(University university);
+  Future<University> getUniversityById(int id);
 }
