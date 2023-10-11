30fc201b592b3937673dc392c12a00fdd0681d66
Basak <chgy2131@naver.com>
2023-10-11 15:47:18 +0900

    feat: [#293] WebView 역할 위젯 생성

---
+++ lib/src/presentation/component/webview_fullscreen.dart
@@ -0,0 +1,44 @@
+import 'package:dart_flutter/res/config/size_config.dart';
+import 'package:dart_flutter/src/common/util/analytics_util.dart';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewFullScreen extends StatefulWidget {
+  final String url;
+  final String title;
+  const WebViewFullScreen({Key? key, required this.url, this.title = ''}) : super(key: key);
+
+  @override
+  State<WebViewFullScreen> createState() => _WebViewFullScreenState();
+}
+
+class _WebViewFullScreenState extends State<WebViewFullScreen> {
+  late WebViewController _webViewController;
+
+  @override
+  void initState() {
+    super.initState();
+    _webViewController = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse(widget.url));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(widget.title),
+        leading: const BackButton(
+          color: Colors.black,
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(SizeConfig.defaultSize * 2),
+          child: WebViewWidget(controller: _webViewController),
+        ),
+      ),
+    );
+  }
+}
