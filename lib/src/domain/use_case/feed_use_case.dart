f6b118d9dfd1ddcb169862f97c4694dc29b97fd1
SeyeonJang <seyeon.sc82@catholic.ac.kr>
2023-11-04 23:58:53 +0900

    fix: [#345] Post(글쓰기) 아이템 삭제에 따른 Entity 제거

--- lib/src/domain/use_case/feed_use_case.dart
+++ lib/src/domain/use_case/feed_use_case.dart
@@ -2,7 +2,6 @@ import 'package:dart_flutter/src/common/pagination/pagination.dart';
 import 'package:dart_flutter/src/domain/entity/comment.dart';
 import 'package:dart_flutter/src/domain/entity/option.dart';
 import 'package:dart_flutter/src/domain/entity/personal_info.dart';
-import 'package:dart_flutter/src/domain/entity/post.dart';
 import 'package:dart_flutter/src/domain/entity/survey.dart';
 import 'package:dart_flutter/src/domain/entity/type/IdCardVerificationStatus.dart';
 import 'package:dart_flutter/src/domain/entity/university.dart';
@@ -17,86 +16,6 @@ class FeedUseCase {
       pickedOption: 1
   );
 
-  Pagination<Post> posts = Pagination(content: [
-    Post(
-        id: 10,
-        title: '타이틀1',
-        content: '본문1',
-        writer: User(
-          personalInfo: const PersonalInfo(
-              id: 123,
-              name: '',
-              nickname: '',
-              profileImageUrl: '',
-              verification: IdCardVerificationStatus.NOT_VERIFIED_YET,
-              phone: '',
-              gender: '',
-              birthYear: 2002,
-              admissionYear: 2022,
-              recommendationCode: 'ABCD1234',
-              point: 0
-          ),
-            university: University(
-                id: 10000,
-                name: '가톨릭대학교',
-                department: ''
-            ),
-            titleVotes: []),
-        comments: [],
-        createdAt: DateTime.now().subtract(Duration(minutes: 2)),
-        likes: 0,
-        liked: false
-    ),
-    Post(
-        id: 11,
-        title: '타이틀2',
-        content: '본문2',
-        writer: User(
-            personalInfo: const PersonalInfo(
-                id: 234,
-                name: '',
-                nickname: '',
-                profileImageUrl: '',
-                verification: IdCardVerificationStatus.NOT_VERIFIED_YET,
-                phone: '',
-                gender: '',
-                birthYear: 2002,
-                admissionYear: 2022,
-                recommendationCode: 'BCDE4321',
-                point: 0
-            ),
-            university: University(
-                id: 10001,
-                name: '인하대학교',
-                department: ''
-            ),
-            titleVotes: []),
-        comments: [Comment(id: 999, writer: User(
-            personalInfo: const PersonalInfo(
-                id: 123,
-                name: '',
-                nickname: '',
-                profileImageUrl: '',
-                verification: IdCardVerificationStatus.NOT_VERIFIED_YET,
-                phone: '',
-                gender: '',
-                birthYear: 2002,
-                admissionYear: 2022,
-                recommendationCode: 'ABCD1234',
-                point: 0
-            ),
-            university: University(
-                id: 10000,
-                name: '가톨릭대학교',
-                department: ''
-            ),
-            titleVotes: []), content: '댓글입니다1', createdAt: DateTime.now())],
-        createdAt: DateTime.now().subtract(Duration(minutes: 1)),
-        likes: 5,
-        liked: true
-    ),
-  ]);
-
   Survey getSurvey() { // 오늘의 질문 get
     return dummySurvey;
   }
@@ -109,13 +28,5 @@ class FeedUseCase {
         picked: true,
         pickedOption: 1
     );
-  }
-
-  Pagination<Post> getPosts({int page = 0, int size = 10}) { // 커뮤니티 글 get (Pagination)
-    return posts;
-  }
-
-  void postOnePost(Post onePost) { // 커뮤니티 글 post
-    posts.content?.add(onePost);
   }
 }
--- lib/src/presentation/feed/viewmodel/feed_cubit.dart
+++ lib/src/presentation/feed/viewmodel/feed_cubit.dart
@@ -3,7 +3,6 @@ import 'package:dart_flutter/src/common/util/analytics_util.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:dart_flutter/src/domain/entity/survey.dart';
 import 'package:dart_flutter/src/domain/entity/user.dart';
-import 'package:dart_flutter/src/domain/entity/post.dart';
 import 'package:dart_flutter/src/domain/use_case/user_use_case.dart';
 import 'package:dart_flutter/src/domain/use_case/feed_use_case.dart';
 import 'package:dart_flutter/src/presentation/feed/viewmodel/state/feed_state.dart';
@@ -15,7 +14,7 @@ class FeedCubit extends Cubit<FeedState> {
 
   // pagination
   static const int _numberOfPostsPerRequest = 10;
-  final PagingController<int, Post> pagingController = PagingController(firstPageKey: 0);
+  // final PagingController<int, Post> pagingController = PagingController(firstPageKey: 0);
 
   void initFeed() async {
     state.setIsLoading(true);
@@ -30,38 +29,38 @@ class FeedCubit extends Cubit<FeedState> {
     emit(state.copy());
   }
 
-  // TODO : Future<void> & getPosts에 await
-  void fetchPage(int pageKey) async {
-    try {
-      final posts = (_feedUseCase.getPosts(page: pageKey, size: _numberOfPostsPerRequest).content ?? []);
-      final isLastPage = posts.length < _numberOfPostsPerRequest;
-      if (isLastPage) {
-        pagingController.appendLastPage(posts);
-      } else {
-        final nextPageKey = pageKey + 1;
-        AnalyticsUtil.logEvent('피드_게시글 불러오기(페이지네이션)', properties: {
-          '새로 불러온 페이지 인덱스': nextPageKey
-        });
-        pagingController.appendPage(posts, nextPageKey);
-      }
-    } catch (error) {
-      print('[PAGINATION ERROR] $error');
-      pagingController.error = error;
-    }
-  }
+  // // TODO : Future<void> & getPosts에 await
+  // void fetchPage(int pageKey) async {
+  //   try {
+  //     final posts = (_feedUseCase.getPosts(page: pageKey, size: _numberOfPostsPerRequest).content ?? []);
+  //     final isLastPage = posts.length < _numberOfPostsPerRequest;
+  //     if (isLastPage) {
+  //       pagingController.appendLastPage(posts);
+  //     } else {
+  //       final nextPageKey = pageKey + 1;
+  //       AnalyticsUtil.logEvent('피드_게시글 불러오기(페이지네이션)', properties: {
+  //         '새로 불러온 페이지 인덱스': nextPageKey
+  //       });
+  //       pagingController.appendPage(posts, nextPageKey);
+  //     }
+  //   } catch (error) {
+  //     print('[PAGINATION ERROR] $error');
+  //     pagingController.error = error;
+  //   }
+  // }
 
   // TODO : Future<void> & async, await
-  void postOnePost(String title, String content) {
-    Post newPost = Post(
-        id: 0,
-        title: title,
-        content: content,
-        writer: state.userResponse,
-        comments: [],
-        createdAt: DateTime.now(),
-        likes: 0,
-        liked: false
-    );
-    _feedUseCase.postOnePost(newPost);
-  }
+  // void postOnePost(String title, String content) {
+  //   Post newPost = Post(
+  //       id: 0,
+  //       title: title,
+  //       content: content,
+  //       writer: state.userResponse,
+  //       comments: [],
+  //       createdAt: DateTime.now(),
+  //       likes: 0,
+  //       liked: false
+  //   );
+  //   _feedUseCase.postOnePost(newPost);
+  // }
 }
