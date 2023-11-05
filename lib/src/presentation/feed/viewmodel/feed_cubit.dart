6b790ed37d7b30b9a0eb7121f5838536916f8c23
SeyeonJang <seyeon.sc82@catholic.ac.kr>
2023-11-05 22:10:32 +0900

    feat: [#345] ViewModel - 오늘의 질문 및 댓글 Cubit, State 추가

--- lib/src/presentation/feed/viewmodel/feed_cubit.dart
+++ lib/src/presentation/feed/viewmodel/feed_cubit.dart
@@ -2,6 +2,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:dart_flutter/src/common/util/analytics_util.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:dart_flutter/src/domain/entity/survey.dart';
+import 'package:dart_flutter/src/domain/entity/survey_detail.dart';
 import 'package:dart_flutter/src/domain/entity/user.dart';
 import 'package:dart_flutter/src/domain/use_case/user_use_case.dart';
 import 'package:dart_flutter/src/domain/use_case/feed_use_case.dart';
@@ -14,7 +15,7 @@ class FeedCubit extends Cubit<FeedState> {
 
   // pagination
   static const int _numberOfPostsPerRequest = 10;
-  // final PagingController<int, Post> pagingController = PagingController(firstPageKey: 0);
+  final PagingController<int, Survey> pagingController = PagingController(firstPageKey: 0);
 
   void initFeed() async {
     state.setIsLoading(true);
@@ -22,45 +23,59 @@ class FeedCubit extends Cubit<FeedState> {
 
     User userResponse = await _userUseCase.myInfo();
     state.setMyInfo(userResponse);
-    Survey survey = await _feedUseCase.getSurvey();
-    state.setSurvey(survey);
 
     state.setIsLoading(false);
     emit(state.copy());
   }
 
-  // // TODO : Future<void> & getPosts에 await
-  // void fetchPage(int pageKey) async {
-  //   try {
-  //     final posts = (_feedUseCase.getPosts(page: pageKey, size: _numberOfPostsPerRequest).content ?? []);
-  //     final isLastPage = posts.length < _numberOfPostsPerRequest;
-  //     if (isLastPage) {
-  //       pagingController.appendLastPage(posts);
-  //     } else {
-  //       final nextPageKey = pageKey + 1;
-  //       AnalyticsUtil.logEvent('피드_게시글 불러오기(페이지네이션)', properties: {
-  //         '새로 불러온 페이지 인덱스': nextPageKey
-  //       });
-  //       pagingController.appendPage(posts, nextPageKey);
-  //     }
-  //   } catch (error) {
-  //     print('[PAGINATION ERROR] $error');
-  //     pagingController.error = error;
-  //   }
-  // }
+  // TODO : Future<void> & getSurveys에 await
+  void fetchPage(int pageKey) async {
+    try {
+      final posts = (_feedUseCase.getSurveys(page: pageKey, size: _numberOfPostsPerRequest).content ?? []);
+      final isLastPage = posts.length < _numberOfPostsPerRequest;
+      if (isLastPage) {
+        pagingController.appendLastPage(posts);
+      } else {
+        final nextPageKey = pageKey + 1;
+        AnalyticsUtil.logEvent('피드_게시글 불러오기(페이지네이션)', properties: {
+          '새로 불러온 페이지 인덱스': nextPageKey
+        });
+        pagingController.appendPage(posts, nextPageKey);
+      }
+    } catch (error) {
+      print('[PAGINATION ERROR] $error');
+      pagingController.error = error;
+    }
+  }
+
+  Future<void> getSurveyDetail(int id) async {
+    SurveyDetail survey = await _feedUseCase.getSurvey(id);
+    state.setSurvey(survey);
+    emit(state.copy());
+  }
+
+  void pickOption() {
+    // TODO : pickOption
+    // View 먼저 바꾸고 서버 전달
+  }
+
 
   // TODO : Future<void> & async, await
-  // void postOnePost(String title, String content) {
-  //   Post newPost = Post(
-  //       id: 0,
-  //       title: title,
-  //       content: content,
-  //       writer: state.userResponse,
-  //       comments: [],
-  //       createdAt: DateTime.now(),
-  //       likes: 0,
-  //       liked: false
-  //   );
-  //   _feedUseCase.postOnePost(newPost);
-  // }
+  void postComment(String content) {
+    _feedUseCase.postComment(state.userResponse, content, DateTime.now());
+  }
+
+  void deleteComment(int commentId) {
+    _feedUseCase.deleteComment(commentId);
+  }
+
+  void reportComment(int commentId) {
+    // TODO : report
+    // View에서 Toast
+  }
+
+  void postLikeComment(int commentId) {
+    // TODO : like
+    // View에서 좋아요
+  }
 }
--- lib/src/presentation/feed/viewmodel/state/feed_state.dart
+++ lib/src/presentation/feed/viewmodel/state/feed_state.dart
@@ -1,29 +1,29 @@
-import 'package:dart_flutter/src/domain/entity/option.dart';
 import 'package:dart_flutter/src/domain/entity/survey.dart';
+import 'package:dart_flutter/src/domain/entity/survey_detail.dart';
 import 'package:dart_flutter/src/domain/entity/user.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 @JsonSerializable()
 class FeedState{
   late bool isLoading;
-  late Survey survey;
+  late SurveyDetail surveyDetail;
   late User userResponse;
 
   FeedState({
     required this.isLoading,
-    required this.survey,
+    required this.surveyDetail,
     required this.userResponse
   });
 
   FeedState.init() {
     isLoading = false;
-    survey = Survey(id: 0, question: '', options: [], picked: false, pickedOption: 0);
+    surveyDetail = SurveyDetail(id: 0, question: '', options: [], picked: false, pickedOption: 0, createdAt: DateTime.now(), latestComment: '', comments: []);
     userResponse = User(personalInfo: null, university: null, titleVotes: []);
   }
 
   FeedState copy() => FeedState(
     isLoading: isLoading,
-    survey: survey,
+    surveyDetail: surveyDetail,
     userResponse: userResponse
   );
 
@@ -31,8 +31,8 @@ class FeedState{
     this.isLoading = isLoading;
   }
 
-  void setSurvey(Survey survey) {
-    this.survey = survey;
+  void setSurvey(SurveyDetail surveyDetail) {
+    this.surveyDetail = surveyDetail;
   }
 
   void setMyInfo(User userResponse) {
