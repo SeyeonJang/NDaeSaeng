5db8f97f4cb8c34650adf1f59bdb189d3ec14c47
SeyeonJang <seyeon.sc82@catholic.ac.kr>
2023-11-07 03:16:45 +0900

    feat: [#345] 선택지 Component - 비율 그래프, 색상 추가

--- lib/src/presentation/feed/view/component/option_component.dart
+++ lib/src/presentation/feed/view/component/option_component.dart
@@ -1,13 +1,49 @@
+import 'package:dart_flutter/res/config/size_config.dart';
 import 'package:dart_flutter/src/domain/entity/option.dart';
 import 'package:flutter/material.dart';
 
 class OptionComponent extends StatelessWidget {
   late Option option;
+  late double percent;
+  late bool isMost;
+  Color mainColor = const Color(0xffFE6059);
+  Color subColor = const Color(0xffFFE0B2);
 
-  OptionComponent({super.key, required this.option});
+  OptionComponent({super.key,
+    required this.option,
+    required this.percent,
+    required this.isMost});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      width: SizeConfig.screenWidth,
+      height: SizeConfig.defaultSize * 4.5,
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(9)),
+        border: Border.all(color: isMost ? mainColor : Colors.grey.shade300)
+      ),
+      child: Row(
+        children: [
+          Container(
+            width: SizeConfig.screenWidth * 0.77 * percent,
+            height: SizeConfig.defaultSize * 4.5,
+            decoration: BoxDecoration(
+                color: isMost ? mainColor.withOpacity(0.7) : subColor,
+                borderRadius: const BorderRadius.only(topLeft: Radius.circular(9), bottomLeft: Radius.circular(9))
+            ),
+            child: Padding(
+              padding: EdgeInsets.only(left: SizeConfig.defaultSize * 1.5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(option.name),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
