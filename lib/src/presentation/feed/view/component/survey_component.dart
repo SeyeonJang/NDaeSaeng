bed0a0e58cad5caf8624b58873a3d6248867f708
SeyeonJang <seyeon.sc82@catholic.ac.kr>
2023-11-07 03:16:56 +0900

    feat: [#345] 오늘의 질문 Component - 선택지, 최근 댓글 추가

--- lib/src/presentation/feed/view/component/survey_component.dart
+++ lib/src/presentation/feed/view/component/survey_component.dart
@@ -1,4 +1,6 @@
 import 'package:dart_flutter/src/domain/entity/survey.dart';
+import 'package:dart_flutter/src/presentation/feed/view/component/option_component.dart';
+import 'package:dart_flutter/src/presentation/feed/view/survey_detail_view.dart';
 import 'package:flutter/material.dart';
 import 'package:dart_flutter/res/config/size_config.dart';
 import 'package:intl/intl.dart';
@@ -6,6 +8,7 @@ import 'package:intl/intl.dart';
 class SurveyComponent extends StatelessWidget {
   late Survey survey;
   Color mainColor = const Color(0xffFE6059);
+  Color commentColor = const Color(0xffFFFAF9);
   double marginHorizontal = SizeConfig.defaultSize * 2.3;
 
   SurveyComponent({super.key, required this.survey});
@@ -15,6 +18,8 @@ class SurveyComponent extends StatelessWidget {
     DateTime now = DateTime.now();
     String formattedSurveyDate = DateFormat('MM월 dd일').format(survey.createdAt);
     String formattedNowDate = DateFormat('MM월 dd일').format(now);
+    double optionFirstPercent = survey.options.first.headCount / (survey.options.first.headCount + survey.options.last.headCount);
+    double optionSecondPercent = survey.options.last.headCount / (survey.options.first.headCount + survey.options.last.headCount);
 
     return Container(
       width: SizeConfig.screenWidth,
@@ -28,38 +33,80 @@ class SurveyComponent extends StatelessWidget {
           )
       ),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-            SizedBox(height: SizeConfig.defaultSize * 2),
           Padding(
             padding: EdgeInsets.symmetric(horizontal: marginHorizontal),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            child: Column(
               children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: mainColor.withOpacity(0.1),
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(
-                        horizontal: SizeConfig.defaultSize,
-                        vertical: SizeConfig.defaultSize * 0.4),
-                    child: Text(formattedSurveyDate, style: TextStyle(
-                        color: mainColor,
-                        fontSize: SizeConfig.defaultSize * 1.4)),
-                  )
+                  SizedBox(height: SizeConfig.defaultSize * 2,),
+                Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                            decoration: BoxDecoration(
+                              color: mainColor.withOpacity(0.1),
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: SizeConfig.defaultSize,
+                                  vertical: SizeConfig.defaultSize * 0.4),
+                              child: Text(formattedSurveyDate, style: TextStyle(
+                                  color: mainColor,
+                                  fontSize: SizeConfig.defaultSize * 1.4)),
+                            )
+                        ),
+                        if (formattedNowDate == formattedSurveyDate) Text("오늘의 질문", style: TextStyle(fontSize: SizeConfig.defaultSize * 1.4, color: Colors.grey),)
+                      ],
+                    ),
+                      SizedBox(height: SizeConfig.defaultSize * 1.5,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(' ${survey.question}', style: TextStyle(fontSize: SizeConfig.defaultSize * 1.6, fontWeight: FontWeight.w600),),
+                      ],
+                    ),
+                      SizedBox(height: SizeConfig.defaultSize * 2,),
+                    OptionComponent(option: survey.options.first, percent: optionFirstPercent, isMost: optionFirstPercent>optionSecondPercent,),
+                      SizedBox(height: SizeConfig.defaultSize),
+                    OptionComponent(option: survey.options.last, percent: optionSecondPercent, isMost: optionFirstPercent<optionSecondPercent,)
+                  ],
                 ),
-                if (formattedNowDate == formattedSurveyDate) Text("오늘의 질문", style: TextStyle(fontSize: SizeConfig.defaultSize * 1.4, color: Colors.grey),)
               ],
             ),
           ),
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: SizeConfig.defaultSize * 1.5, horizontal: marginHorizontal),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text(' ${survey.question}', style: TextStyle(fontSize: SizeConfig.defaultSize * 1.6, fontWeight: FontWeight.w600),),
-              ],
+
+          GestureDetector(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => const SurveyDetailView()));
+            },
+            child: Container(
+              width: SizeConfig.screenWidth,
+              height: SizeConfig.defaultSize * 6.5,
+              color: commentColor,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: marginHorizontal),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('실시간 댓글', style: TextStyle(color: mainColor, fontSize: SizeConfig.defaultSize * 1.3),),
+                        Icon(Icons.arrow_forward_ios_rounded, size: SizeConfig.defaultSize * 1.3, color: mainColor,)
+                      ],
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: SizeConfig.defaultSize * 0.5),
+                      child: Text(survey.latestComment, style: TextStyle(fontSize: SizeConfig.defaultSize * 1.3), overflow: TextOverflow.ellipsis,),
+                    )
+                  ],
+                ),
+              ),
             ),
           )
         ],
---
+++ lib/src/presentation/feed/view/survey_detail_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SurveyDetailView extends StatelessWidget {
+  const SurveyDetailView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
