4e4b4a53ddbddd5b2d1adfc4cc37c395a8d6a0cf
Basak <chgy2131@naver.com>
2023-11-15 18:31:22 +0900

    design: [#360] 설문 컴포넌트 하단 모서리가 곡선이 아니던 문제를 수정 (테두리 부분 추가 수정 필요)

--- lib/src/presentation/feed/view/component/survey_component.dart
+++ lib/src/presentation/feed/view/component/survey_component.dart
@@ -99,6 +99,7 @@ class _SurveyComponentState extends State<SurveyComponent> {
     return Container(
       width: SizeConfig.screenWidth,
       height: SizeConfig.defaultSize * 30,
+      clipBehavior: Clip.antiAlias,
       decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.circular(15),
